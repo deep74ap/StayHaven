f152c55c04ea40634c01fd7b9ad22f978d3b4f19
deep74ap <deepak09012004@gmail.com>
2025-03-29 02:39:16 +0530

    Adding user , signup and passport middlewaare

--- app.js
+++ app.js
@@ -12,10 +12,15 @@ const ExpressError = require("./utils/ExpressError.js");
 // const {listingSchema , reviewSchema} = require("./Scema.js"); 
 // const reviewSchema = require("./Scema.js");
 // const review = require("./models/review.js"); 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js")
+const listingRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
 // const Listing = require("./models/listing.js"); 
@@ -58,14 +63,24 @@ app.get("/",(req,res) =>{
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 app.use((req,res,next) =>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewsRouter);
+app.use("/",userRouter);
 
 
 
